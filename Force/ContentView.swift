e8a9c67db6c0747db28f748dfb2530118951f974
Yuting Zhong <izhongyuting@gmail.com>
2026-01-06 17:56:33 -0800

    Initial Commit

---
+++ Force/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Force
+//
+//  Created by Yuting Zhong on 1/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Force/ForceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForceApp.swift
+//  Force
+//
+//  Created by Yuting Zhong on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ForceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
